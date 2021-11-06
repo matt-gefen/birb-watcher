e3f327a71eed44e0207baa6e565317e5f8938e37
mattgefen <matt.gefen@gmail.com>
2021-11-06 14:31:19 -0600

    logging in redirects to your sightings

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
+  // res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
+  const user = req.user ? req.user : null
+  if (user) {
+    res.redirect(`/profiles/${user.profile._id}`)
+  }
+  else {
+    res.render('index', { 
+      title: 'Home Page', 
+      user: user
+    })
+  }
 })
 
 export {
