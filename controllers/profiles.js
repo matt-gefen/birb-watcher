ec0c83527d824941f7756d71cdf2ee3c76dae36f
mattgefen <matt.gefen@gmail.com>
2021-11-06 12:15:27 -0600

    profile index stubbed

---
+++ controllers/profiles.js
@@ -0,0 +1,9 @@
+import {Profile} from "../models/profile.js"
+
+function index(req, res) {
+  console.log('indexed watchers')
+}
+
+export {
+  index
+}
---
+++ routes/profiles.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import * as profilesCtrl from "../controllers/profiles.js"
+
+const router = Router()
+
+router.get('/', profilesCtrl.index)
+
+export {
+  router
+}
