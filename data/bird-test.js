202833a08c7d186ba37a640fd3ece72034a3151e
mattgefen <matt.gefen@gmail.com>
2021-11-10 19:43:52 -0700

    bird species code and name saved to json

--- data/bird-test.js
+++ data/bird-test.js
@@ -2,6 +2,7 @@
 
 // this document is entirely to mess around with the importing of taxonomy info from the ebird api for implementation on the app
 import axios from 'axios'
+import fs from 'fs'
 import { usBirdSpeciesCodes } from './usBirds.js'
 
 const apiUrl = 'https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&species=wfwduc1'
@@ -33,25 +34,71 @@ const apiUrl = 'https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&species=wfw
 
 // getAllSpecies()
 
-async function createBird(speciesCode, quantity) {
-  try {
-    let apiUrl = `https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&species=${speciesCode}`
-    const response = await axios.get(apiUrl)
-    const body = response.data[0]
-    // console.log(body)
-    const bird = {
-      commonName: body.comName,
-      speciesName: body.speciesName,
-      speciesCode: body.speciesCode,
-      familyComName: body.familyComName,
-      cornellLink: `https://ebird.org/species/${body.speciesCode}`,
-      quantity,
-      notes: 'eh'
+// async function createBird(speciesCode, quantity) {
+//   try {
+//     let apiUrl = `https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&species=${speciesCode}`
+//     const response = await axios.get(apiUrl)
+//     const body = response.data[0]
+//     // console.log(body)
+//     const bird = {
+//       commonName: body.comName,
+//       speciesName: body.speciesName,
+//       speciesCode: body.speciesCode,
+//       familyComName: body.familyComName,
+//       cornellLink: `https://ebird.org/species/${body.speciesCode}`,
+//       quantity,
+//       notes: 'eh'
+//     }
+//     return bird
+//   } catch(error) {
+//     console.log(error)
+//   }
+// }
+
+async function getSpeciesNames() {
+  let apiUrl = `https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json`
+  const response = await axios.get(apiUrl)
+  let usBirds = []       
+  response.data.forEach(species => {
+    if(usBirdSpeciesCodes.includes(species.speciesCode)) {
+      usBirds.push({
+        speciesCode: species.speciesCode,
+        comName: species.comName
+      })
     }
-    return bird
+  })
+  return usBirds
+}
+
+async function getBirdJson() {
+  try {
+    let birdFile = await getSpeciesNames()
+    let birdFileString =  JSON.stringify(birdFile)
+    fs.writeFile('bibdata.json', birdFileString, (err) => {
+      if (err) {
+          throw err;
+      }
+      console.log("JSON data is saved.");
+  })
   } catch(error) {
     console.log(error)
   }
 }
 
-console.log(createBird('yebcha',2))
+getBirdJson()
+
+
+// async function getBird(speciesCode) {
+//   try {
+//     let apiUrl = `https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&species=${speciesCode}`
+//     const response = await axios.get(apiUrl)
+//     const body = response.data[0]
+//     const bird = {
+//       commonName: body.comName,
+//       speciesName: body.sciName,
+//     }
+//     return bird
+//   } catch(error) {
+//     console.log(error)
+//   }
+// }
